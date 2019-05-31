f584e29f9535d953b73b7b12ca9d6401a5f61bd4
wangjiehong <wangjiehong@jd.com>
2019-05-31 15:50:52 +0800

    add io

---
+++ io/io.go
@@ -0,0 +1,266 @@
+package io
+
+import "errors"
+
+const (
+	SeekStart = 0
+	SeekCurrent = 1
+	SeekEnd = 2
+)
+
+type Reader interface {
+	Read(p []byte) (n int, err error)
+}
+
+type Writer interface {
+	Write(p []byte) (n int, err error)
+}
+
+type Closer interface {
+	Close() error
+}
+
+type Seeker interface {
+	Seek(offset int64, whence int) (int64, error)
+}
+
+type ReadWriter interface {
+	Reader
+	Writer
+}
+
+type ReadCloser interface {
+	Reader
+	Closer
+}
+
+type ReadSeeker interface {
+	Reader
+	Seeker
+}
+
+type WriteCloser interface {
+	Writer
+	Closer
+}
+
+type WriteSeeker interface {
+	Writer
+	Seeker
+}
+
+type ReadWriteCloser interface {
+	Reader
+	Writer
+	Closer
+}
+
+type ReadWriteSeeker interface {
+	Reader
+	Writer
+	Seeker
+}
+
+type StringWriter interface {
+	WriteString(s string) (n int, err error)
+}
+
+type ReaderFrom interface {
+	ReadFrom(r Reader) (n int64, err error)
+}
+
+type WriterTo interface {
+	WriteTo(w Writer) (n int64, err error)
+}
+
+type ReaderAt interface {
+	ReadAt(p []byte, off int64) (n int, err error)
+}
+
+type WriterAt interface {
+	WriteAt(p []byte, off int64) (n int, err error)
+}
+
+type ByteReader interface {
+	ReadByte() (byte, error)
+}
+
+type ByteScanner interface {
+	ByteReader
+	UnreadByte() error
+}
+
+type ByteWriter interface {
+	WriteByte(c byte) error
+}
+
+type RuneReader interface {
+	ReadRune() (r rune, size int, err error)
+}
+
+type RuneScanner interface {
+	RuneReader
+	UnreadRune() error
+}
+
+type LimitedReader struct {
+	R Reader
+	N int64
+}
+
+func (l *LimitedReader) Read(p []byte) (n int, err error) {
+	if l.N <= 0 {
+		return 0, EOF
+	}
+	if int64(len(p)) > l.N {
+		p = p[0:l.N]
+	}
+	n, err = l.R.Read(p)
+	l.N -= int64(n)
+	return n, err
+}
+
+func LimitReader(r Reader, n int64) Reader { return &LimitedReader{R:r, N: n} }
+
+func WriteString(w Writer, s string) (n int, err error) {
+	if sw, ok := w.(StringWriter); ok {
+		return sw.WriteString(s)
+	}
+	return w.Write([]byte(s))
+}
+
+var EOF = errors.New("EOF")
+var ErrUnexpectedEOF = errors.New("unexpected EOF")
+var ErrShortBuffer = errors.New("short buffer")
+var ErrShortWrite = errors.New("short write")
+
+func ReadAtLeast(r Reader, buf []byte, min int) (n int, err error) {
+	if len(buf) < min {
+		return 0, ErrShortBuffer
+	}
+	for n < min && err == nil {
+		var nn int
+		nn, err = r.Read(buf[n:])
+		n += nn
+	}
+	if n >= min {
+		err = nil
+	} else if n > 0 && err == EOF {
+		err = ErrUnexpectedEOF
+	}
+	return n, err
+}
+
+func ReadFull(r Reader, buf []byte) (n int, err error) {
+	return ReadAtLeast(r, buf, len(buf))
+}
+
+func CopyN(dst Writer, src Reader, n int64) (written int64, err error) {
+	written, err = Copy(dst, LimitReader(src, n))
+	if written == n {
+		return n, nil
+	}
+	if written < n && err == nil {
+		err = EOF
+	}
+	return written, err
+}
+
+func Copy(dst Writer, src Reader) (written int64, err error) {
+	return copyBuffer(dst, src, nil)
+}
+
+func CopyBuffer(dst Writer, src Reader, buf []byte) (written int64, err error) {
+	if buf != nil && len(buf) == 0 {
+		panic("empty buffer in io.CopyBuffer")
+	}
+	return copyBuffer(dst, src, buf)
+}
+
+func copyBuffer(dst Writer, src Reader, buf []byte) (written int64, err error) {
+	if wt, ok := src.(WriterTo); ok {
+		return wt.WriteTo(dst)
+	}
+	if rf, ok := dst.(ReaderFrom); ok {
+		return rf.ReadFrom(src)
+	}
+	if buf == nil {
+		size := 32 * 1024
+		if l, ok := src.(*LimitedReader); ok && int64(size) > l.N {
+			if l.N < 1 {
+				size = 1
+			} else {
+				size = int(l.N)
+			}
+		}
+	}
+	for {
+		nr, er := src.Read(buf)
+		if nr > 0 {
+			nw, ew := dst.Write(buf[0:nr])
+			if nw > 0 {
+				written += int64(nw)
+			}
+			if ew != nil {
+				err = ew
+				break
+			}
+			if nr != nw {
+				err = ErrShortWrite
+			}
+		}
+		if er != nil {
+			if er != EOF {
+				err = er
+			}
+			break
+		}
+	}
+	return written, err
+}
+
+type teeReader struct {
+	r Reader
+	w Writer
+}
+
+func TeeReader(r Reader, w Writer) Reader {
+	return &teeReader{r: r, w: w}
+}
+
+func (t *teeReader) Read(p []byte) (n int, err error) {
+	n, err = t.r.Read(p)
+	if n > 0 {
+		if n, err := t.w.Write(p[:n]); err != nil {
+			return n, err
+		}
+	}
+	return n, err
+}
+
+type SectionReader struct {
+	r ReaderAt
+	base int64
+	off int64
+	limit int64
+}
+
+func NewSectionReader(r ReaderAt, off int64, n int64) *SectionReader {
+	return &SectionReader{r, off, off, off + n}
+}
+
+var errWhence = errors.New("Seek: invalid whence")
+var errOffset = errors.New("Seek: invalid offset")
+
+
+
+
+
+
+
+
+
+
+
+
+
